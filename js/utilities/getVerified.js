35ae33a5462ce69254a0e94bcfbe539083000b5c
Vicente Figueiredo <vicenteppfigueiredo@gmail.com>
2024-05-25 16:07:48 +0100

    updates to api

--- js/utilities/getVerified.js
+++ js/utilities/getVerified.js
@@ -1,7 +1,7 @@
 import verify from "./verify.js";
 
 export default async function getVerified(location, url, verifier) {
-    const response = await fetch(url);
+    const response = await fetch(url + `${url.contains('?') ? '&' : '?'}api_key=HDEV-e3a9be99-1ce4-4f3f-a052-1a83b52c3da5`);
     const responseJson = await response.json();
     if (!verifier) {
         verify(location, responseJson);
